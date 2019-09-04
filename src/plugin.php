f847afa85fd506e0a5158b623509cf901013e67f
hellofromMeow <mklim9866@gmail.com>
2019-09-04 17:12:53 +0800

    initial commit

---
+++ src/plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description.
+ *
+ * @package   ${NAMESPACE}
+ * @since     1.0.0
+ * @author    hellofromMeow
+ * @link      http://example.com
+ * @license   MIT
+ */
+
---
+++ src/shortcode/shortcodes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description.
+ *
+ * @package   ${NAMESPACE}
+ * @since     1.0.0
+ * @author    hellofromMeow
+ * @link      http://example.com
+ * @license   MIT
+ */
